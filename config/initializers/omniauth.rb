b30a0a151320ba59c28e9ed69bb57cf1ea3ba917
Kinartb <arturomho8x@gmail.com>
2023-11-15 01:31:31 -0500

    proyecto finalizado

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, "o8H1Ik5GYaDiwmfwMEy34btlf", "DZyzEyh5tNXVjX51LsbU92IGrTWU9WVmyDUoLK1negYFGx2lXV"
+end
---
+++ db/migrate/20231115060718_create_reviews.rb
@@ -0,0 +1,11 @@
+# db/migrate/*_create_reviews.rb
+class CreateReviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table 'reviews' do |t|
+      t.integer    'potatoes'
+      t.text       'comments'
+      t.references 'moviegoer'
+      t.references 'movie'
+    end
+  end
+end
